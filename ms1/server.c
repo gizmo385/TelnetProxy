53362784ee7cb2879885cf07f82b9eb8ed85dcf5
Helen Louise Jones <helenjones@cs.arizona.edu>
2016-02-04 20:44:57 -0700

    First Draft of server code

--- ms1/server.c
+++ ms1/server.c
@@ -15,6 +15,7 @@ int main(int argc, char* argv[]){
 	}
 
 	struct sockaddr_in *my_addr = calloc(1, sizeof(sockaddr_in));
+	char buf[1024];
 
 		// 1. Create a socket with the socket() system call
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,9 +24,18 @@ int main(int argc, char* argv[]){
 		exit(1);
 	}
 	
-		// 2. Bind the socket to an address using bind() system call.
-	//int port = atoi(argv[1]);
-	int bindfd = bind(sockfd, my_addr, sizeof(sockaddr_in));
+		// 2. Bind the socket to an address using bind() system call.i
+	//assign the address family of the server
+	my_addr.sin_family = AF_INET;
+
+	//convert the port to the network byte order specified
+	int port = atoi(argv[1]);
+	my_addr.sin_port = htons(port);
+
+	//assign the server's ip
+	my_addr.sin_addr.s_addr = INADDR_ANY;
+	
+	int bindfd = bind(sockfd, &my_addr, sizeof(sockaddr_in));
 	if(bindfd == -1){
 		fprintf(stderr, "Error: Binding failed\n");
 		exit(1);
@@ -40,8 +50,19 @@ int main(int argc, char* argv[]){
 
 	while(1){
 		//do stuff with the client 
+		int connection = accept(listenrt, (struct sockaddr*) NULL, sizeof(sockaddr_in));
+		while(char *c != EOF){
+			//1. Print the 1st line (size)
+			int len = recv(connection, buf, sizeof(buf), 0);
+			//2. print the payload test	
+			//This is def wrong
+			c = recv(connection, bug, sizeof(buf), 0);
+			printf("%s\n", c);
+		}
 
 		// close the client
+		close();
+
 	}
 /*
 	4. Accept a connection with accept() -- blocks until a client connects with the server
