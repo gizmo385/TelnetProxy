8f431a7d1ce85eee1b3c149c079e3821c5953c68
Helen Louise Jones <helenjones@cs.arizona.edu>
2016-02-06 14:44:04 -0700

    WOW THIS SERVER.C COMPILES

--- ms1/server.c
+++ ms1/server.c
@@ -1,12 +1,16 @@
 //Helen's Server machine is vm60
 //Helen's Socket machine is vm61
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <strings.h>
+
+#define MAX_LINE 1024
+#define MAX_PENDING 5
 
 int main(int argc, char* argv[]){
 
@@ -15,17 +19,20 @@ int main(int argc, char* argv[]){
 		exit(1);
 	}
 
-	struct sockaddr_in *my_addr = calloc(1, sizeof(sockaddr_in));
-	char buf[1024];
+	struct sockaddr_in my_addr;
+	char buf[MAX_LINE];
+	int len;			//length of 
 
-		// 1. Create a socket with the socket() system call
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero((char *)&my_addr, sizeof(my_addr));
+
+		// Create a socket with the socket() system call
+	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
 	if(sockfd == -1){
 		fprintf(stderr, "Error: Socket creation failed\n");
 		exit(1);
 	}
 	
-		// 2. Bind the socket to an address using bind() system call.i
+		//Bind the socket to an address using bind() system call
 	//assign the address family of the server
 	my_addr.sin_family = AF_INET;
 
@@ -36,14 +43,14 @@ int main(int argc, char* argv[]){
 	//assign the server's ip
 	my_addr.sin_addr.s_addr = INADDR_ANY;
 	
-	int bindfd = bind(sockfd, &my_addr, sizeof(sockaddr_in));
+	int bindfd = bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr));
 	if(bindfd == -1){
 		fprintf(stderr, "Error: Binding failed\n");
 		exit(1);
 	}
 	
-		// 3. Listen for connections with listen()
-	int listenrt = listen(sockfd, 5);
+		//Listen for connections with listen()
+	int listenrt = listen(sockfd, MAX_PENDING);
 	if(listenrt == -1){
 		fprintf(stderr, "Error: Listen call failed\n");
 		exit(1);
@@ -51,18 +58,18 @@ int main(int argc, char* argv[]){
 
 	while(1){
 		//do stuff with the client 
-		int connection = accept(listenrt, (struct sockaddr*) NULL, sizeof(sockaddr_in));
-		while(char *c != EOF){
-			//1. Print the 1st line (size)
-			int len = recv(connection, buf, sizeof(buf), 0);
-			//2. print the payload test	
-			//This is def wrong
-			c = recv(connection, bug, sizeof(buf), 0);
-			printf("%s\n", c);
+		int connection = accept(sockfd, (struct sockaddr *)&my_addr, &len);
+		len = recv(connection, buf, sizeof(buf), 0);
+		while(len){
+			//1st arg (size)
+			len = recv(connection, buf, sizeof(buf), 0);
+			// payload arg	
+			len = recv(connection, buf, sizeof(buf), 0);
+			printf("%s\n", buf);
 		}
 
 		// close the client
-		close();
+		close(connection);
 
 	}
 /*
