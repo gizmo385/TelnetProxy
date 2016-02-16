0cffef848979c8cdf8e40d78be6bf60aa093886c
Christopher Chapline <cachapline8@gmail.com>
2016-02-15 17:45:37 -0700

    Stub out milestone 2 files

---
+++ ms2/cproxy.c
@@ -0,0 +1,24 @@
+/*
+CSC 425 Assignment 2
+
+Authors: Chris Chapline and Helen Jones
+
+Due Date: Wednesday, February 24th
+
+cproxy.c: establishes a connection with the server and sends ASCII-based messages.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>	//for closing the connection
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+
+int main(int arc, char *argv[]) {
+
+}
---
+++ ms2/sproxy.c
@@ -0,0 +1,24 @@
+/*
+CSC 425 Assignment 1
+
+Authors: Chris Chapline and Helen Jones
+
+Due Date: Wednesday, February 24th
+
+Server.c -- creates an open server that waits for connections
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>	//for closing the connection
+#include <stdlib.h>
+#include <strings.h>
+
+
+int main(int arc, char *argv[]) {
+
+}
