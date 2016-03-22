8c46174f0c50c8662c5a04c13de566d783e485db
Christopher Chapline <cachapline8@gmail.com>
2016-03-22 04:01:10 +0000

    Return null from read_message if a read failed

--- ms3/protocol.c
+++ ms3/protocol.c
@@ -10,6 +10,7 @@
 #include <unistd.h>	//for closing the connection
 
 #include "protocol.h"
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
 void send_message(int socket, message_t *message) {
 
@@ -63,16 +64,20 @@ void send_message(int socket, message_t *message) {
     }
 }
 
+void safe_read(int socket, char *buffer, size_t size) {
+    // TODO: Ensure complete read
+}
+
 message_t *read_message(int socket) {
     uint32_t n_message_flag;
-    read(socket, (char *) &n_message_flag, sizeof(uint32_t));
+    int result = read(socket, (char *) &n_message_flag, sizeof(uint32_t));
     int message_flag = ntohl(n_message_flag);
 
     printf("Read message with flag %d\n from socket %d\n", message_flag, socket);
 
     switch(message_flag) {
         case HEARTBEAT_FLAG:
-            return new_heartbeat_message();
+            return result ? new_heartbeat_message() : NULL;
             break;
         case CONNECTION_FLAG:
             {
@@ -80,30 +85,33 @@ message_t *read_message(int socket) {
                 char *old_ip = calloc(IP_SIZE, sizeof(char));
                 char *new_ip = calloc(IP_SIZE, sizeof(char));
 
-                read(socket, (char *) &seq_num, sizeof(uint32_t));
-                read(socket, (char *) &ack_num, sizeof(uint32_t));
-                read(socket, (char *) old_ip, sizeof(char) * IP_SIZE);
-                read(socket, (char *) new_ip, sizeof(char) * IP_SIZE);
+                result = MIN(result, read(socket, (char *) &seq_num, sizeof(uint32_t)));
+                result = MIN(result, read(socket, (char *) &ack_num, sizeof(uint32_t)));
+                result = MIN(result, read(socket, (char *) old_ip, sizeof(char) * IP_SIZE));
+                result = MIN(result, read(socket, (char *) new_ip, sizeof(char) * IP_SIZE));
 
-                return new_conn_message(ntohl(seq_num), ntohl(ack_num), old_ip, new_ip);
+                seq_num = ntohl(seq_num);
+                ack_num = ntohl(ack_num);
+
+                return result ? new_conn_message(seq_num, ack_num, old_ip, new_ip) : NULL;
                 break;
             }
         case DATA_FLAG:
             {
                 uint32_t message_size, seq_num, ack_num;
 
-                read(socket, (char *) &message_size, sizeof(uint32_t));
-                read(socket, (char *) &seq_num, sizeof(uint32_t));
-                read(socket, (char *) &ack_num, sizeof(uint32_t));
+                result = MIN(result, read(socket, (char *) &message_size, sizeof(uint32_t)));
+                result = MIN(result, read(socket, (char *) &seq_num, sizeof(uint32_t)));
+                result = MIN(result, read(socket, (char *) &ack_num, sizeof(uint32_t)));
 
                 message_size = ntohl(message_size);
                 seq_num = ntohl(seq_num);
                 ack_num = ntohl(ack_num);
 
                 char *payload = calloc(message_size, sizeof(char));
-                read(socket, payload, message_size);
+                result = MIN(result, read(socket, payload, message_size));
 
-                return new_data_message(seq_num, ack_num, message_size, payload);
+                return result ? new_data_message(seq_num, ack_num, message_size, payload) : NULL;
                 break;
             }
         default:
